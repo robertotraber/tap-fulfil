0462a11e7b00bcf5b84764751dfa1ef82e0d3498
Prakash Pandey <prakash.pandey@fulfil.io>
2018-10-22 14:09:52 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-fulfil",
-    version="0.1.6",
+    version="0.1.7",
     description="Singer.io tap for extracting data",
     author="Fulfil.IO Inc.",
     url="https://www.fulfil.io",
